074ad7e9c919cd017eef4ba2a476432e00a0cf67
akorycki <43526803+akorycki@users.noreply.github.com>
2018-09-25 20:55:56 -0700

    Update KYPDscanner.py

--- KYPDscanner.py
+++ KYPDscanner.py
@@ -6,113 +6,105 @@ GPIO.setwarnings(False)
 class KYPDscanner:
 
     def __init__(self, COL4, COL3, COL2, COL1, ROW4, ROW3, ROW2, ROW1):
-        GPIO.setup(8, GPIO.OUT)  #COL 4
-        GPIO.setup(10, GPIO.OUT) #COL 3
-        GPIO.setup(12, GPIO.OUT) #COL 2
-        GPIO.setup(16, GPIO.OUT) #COL 1
-        GPIO.setup(18, GPIO.IN)  #ROW 4
-        GPIO.setup(22, GPIO.IN)  #ROW 3
-        GPIO.setup(24, GPIO.IN)  #ROW 2
-        GPIO.setup(26, GPIO.IN)  #ROW 1
+        self.COL4 = COL4
+        self.COL3 = COL3
+        self.COL2 = COL2
+        self.COL1 = COL1
+        self.ROW4 = ROW4
+        self.ROW3 = ROW3
+        self.ROW2 = ROW2
+        self.ROW1 = ROW1
+        
+        GPIO.setup(COL4, GPIO.OUT)  #COL 4
+        GPIO.setup(COL3, GPIO.OUT) #COL 3
+        GPIO.setup(COL2, GPIO.OUT) #COL 2
+        GPIO.setup(COL1, GPIO.OUT) #COL 1
+        GPIO.setup(ROW4, GPIO.IN)  #ROW 4
+        GPIO.setup(ROW3, GPIO.IN)  #ROW 3
+        GPIO.setup(ROW2, GPIO.IN)  #ROW 2
+        GPIO.setup(ROW1, GPIO.IN)  #ROW 1
 
-        GPIO.output(8, True)
-        GPIO.output(10, True)
-        GPIO.output(12, True)
-        GPIO.output(16, True)
+        GPIO.output(self.COL4, True)
+        GPIO.output(self.COL3, True)
+        GPIO.output(self.COL2, True)
+        GPIO.output(self.COL1, True)
 
 
     def scan(self):
         while True:
-            GPIO.output(8, False)
-            if not GPIO.input(18):
-                GPIO.output(8, True)
-                time.sleep(0.15)
+            GPIO.output(self.COL4, False)
+            if not GPIO.input(self.ROW4):
+                GPIO.output(self.COL4, True)
                 return ('D')
-            if not GPIO.input(22):
-                GPIO.output(8, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW3):
+                GPIO.output(self.COL4, True)
                 return ('C')
-            if not GPIO.input(24):
-                GPIO.output(8, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW2):
+                GPIO.output(self.COL4, True)
                 return ('B')
-            if not GPIO.input(26):
-                GPIO.output(8, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW1):
+                GPIO.output(self.COL4, True)
                 return ('A')
-            GPIO.output(8, True)
+            GPIO.output(self.COL4, True)
 
-            GPIO.output(10, False)
-            if not GPIO.input(18):
-                GPIO.output(10, True)
-                time.sleep(0.15)
+            GPIO.output(self.COL3, False)
+            if not GPIO.input(self.ROW4):
+                GPIO.output(self.COL3, True)
                 return ('E')
-            if not GPIO.input(22):
-                GPIO.output(10, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW3):
+                GPIO.output(self.COL3, True)
                 return ('9')
-            if not GPIO.input(24):
-                GPIO.output(10, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW2):
+                GPIO.output(self.COL3, True)
                 return ('6')
-            if not GPIO.input(26):
-                GPIO.output(10, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW1):
+                GPIO.output(self.COL3, True)
                 return ('3')
-            GPIO.output(10, True)
+            GPIO.output(self.COL3, True)
 
-            GPIO.output(12, False)
-            if not GPIO.input(18):
-                GPIO.output(12, True)
-                time.sleep(0.15)
+            GPIO.output(self.COL2, False)
+            if not GPIO.input(self.ROW4):
+                GPIO.output(self.COL2, True)
                 return ('F')
-            if not GPIO.input(22):
-                GPIO.output(12, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW3):
+                GPIO.output(self.COL2, True)
                 return ('8')
-            if not GPIO.input(24):
-                GPIO.output(12, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW2):
+                GPIO.output(self.COL2, True)
                 return ('5')
-            if not GPIO.input(26):
-                GPIO.output(12, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW1):
+                GPIO.output(self.COL2, True)
                 return ('2')
-            GPIO.output(12, True)
+            GPIO.output(self.COL2, True)
 
-            GPIO.output(16, False)
-            if not GPIO.input(18):
-                GPIO.output(16, True)
-                time.sleep(0.15)
+            GPIO.output(self.COL1, False)
+            if not GPIO.input(self.ROW4):
+                GPIO.output(self.COL1, True)
                 return ('0')
-            if not GPIO.input(22):
-                GPIO.output(16, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW3):
+                GPIO.output(self.COL1, True)
                 return ('7')
-            if not GPIO.input(24):
-                GPIO.output(16, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW2):
+                GPIO.output(self.COL1, True)
                 return ('4')
-            if not GPIO.input(26):
-                GPIO.output(16, True)
-                time.sleep(0.15)
+            if not GPIO.input(self.ROW1):
+                GPIO.output(self.COL1, True)
                 return ('1')
-            GPIO.output(16, True)
+            GPIO.output(self.COL1, True)
 
-    def begin(self):
+    def echo(self):
         while True:
             print(self.scan())
+            time.sleep(0.2)
+                
         
 
     def string(self):
         word = ''
         while (True):
             hex = self.scan()
+            time.sleep(0.2)
             if ((hex != 'E')):
                 word = word + hex
             elif (hex == 'E'):
                 print(word)
-                
-                
-
-
